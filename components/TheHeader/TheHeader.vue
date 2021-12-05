f7059383e20c2bd630f6e285d8155b78c9ea89d5
Okiemute Gold <goldokiemute1@gmail.com>
2021-12-05 15:47:44 +0100

    file migration, first commit

---
+++ components/TheHeader/TheHeader.vue
@@ -0,0 +1,55 @@
+<template>
+    <header class="main_header">
+        <nav class="main_nav">
+            <ul class="nav_links">
+                <nuxt-link to="/" tag="li" class="nav_link">
+                    <a>All Posts</a>
+                </nuxt-link>
+                <nuxt-link to="/about" tag="li" class="nav_link">
+                    <a>About</a>
+                </nuxt-link>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.main_header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 4.5rem;
+    background: #022d30;
+}
+
+.main_nav {
+    height: 100%;
+}
+
+.nav_links {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    list-style: none;
+}
+
+.nav_link {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 1rem;
+    padding: 0.25rem;
+}
+
+.nav_link a {
+    display: block;
+    text-decoration: none;
+    color: #fff;
+}
+</style>
---
+++ pages/about/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <section id="about_page">
+        <h1>About page</h1>
+        <p>We blog about some anything tech.</p>
+    </section>
+</template>
+<script>
+export default {};
+</script>
+
+<style>
+</style>
